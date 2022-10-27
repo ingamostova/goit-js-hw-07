c291e5f78e6c90af85c7539ed67b4b77a7cb293a
ingamostova <ingo4ka20041997@gmail.com>
2022-10-27 21:03:25 +0300

    1 task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,6 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const galleryContainer = document.querySelector('.gallery');
+let instance = '';
 
 galleryContainer.innerHTML = getGalleryMarkup(galleryItems);
 
@@ -12,10 +13,12 @@ galleryContainer.addEventListener('click', (evt) => {
         return;
     }
     onImageClick(evt);
+    document.addEventListener('keydown', closeModalOnEscape);
 })
 
+
  function onImageClick(evt) {
-    const instance = basicLightbox.create(`
+    instance = basicLightbox.create(`
     <img src="${evt.target.dataset.source}">
 `)
 instance.show()
@@ -35,3 +38,10 @@ function getGalleryMarkup(items) {
 </div>`
 }).join('');
 };
+
+function closeModalOnEscape(evt) {
+    if (evt.code === 'Escape') {
+       instance.close();
+       document.removeEventListener('keydown', closeModalOnEscape)
+   }
+}
