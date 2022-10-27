ca5f8d2ec6147f498e2af456a9774fd55c45b189
ingamostova <ingo4ka20041997@gmail.com>
2022-10-27 20:40:24 +0300

    Виконана частина завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.innerHTML = getGalleryMarkup(galleryItems);
+
+
+galleryContainer.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+    onImageClick(evt);
+})
+
+ function onImageClick(evt) {
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}">
+`)
+instance.show()
+};
+
+function getGalleryMarkup(items) {
+    return items.map(item => {
+   return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+}).join('');
+};
