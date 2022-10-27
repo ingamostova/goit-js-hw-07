9cb1a55e72a43833b508c7b3f719a49caee913f9
ingamostova <ingo4ka20041997@gmail.com>
2022-10-27 21:40:06 +0300

    1 and 2 tasks done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.innerHTML = getGalleryMarkup(galleryItems);
+
+function getGalleryMarkup(items) {
+  return items
+    .map(item => {
+      return `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
